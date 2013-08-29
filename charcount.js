f6a0afa6bd3f2b111bc1c4991247df8b48f7842e
Pintu Kumar <pintukumar_2013@hotmail.com>
2013-08-30 04:49:49 +0530

    Create charcount.js

---
+++ charcount.js
@@ -0,0 +1,114 @@
+<script type="text/javascript">
+// fieldname, warningname, remainingname, maxchars
+function CheckFieldLength(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+     {
+       fn.value = fn.value.substring(0, mc);
+       len = mc;
+       
+    }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+   
+   
+function CheckFieldLength1(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+      {
+        fn.value = fn.value.substring(0, mc);
+        len = mc;
+      }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+
+
+function CheckFieldLength2(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+      {
+        fn.value = fn.value.substring(0, mc);
+        len = mc;
+      }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+
+
+function CheckFieldLength3(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+      {
+        fn.value = fn.value.substring(0, mc);
+        len = mc;
+      }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+
+
+function CheckFieldLength4(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+      {
+        fn.value = fn.value.substring(0, mc);
+        len = mc;
+      }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+
+
+function CheckFieldLength5(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+      {
+        fn.value = fn.value.substring(0, mc);
+        len = mc;
+      }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+
+
+function CheckFieldLength6(fn, wn, rn, mc, ms)
+  {
+    var len = fn.value.length;
+    if (len > mc)
+      {
+        fn.value = fn.value.substring(0, mc);
+        len = mc;
+      }
+    var msg = len / 160 ;
+    var msg1 = Math.ceil(msg);
+    document.getElementById(wn).innerHTML = len;
+    document.getElementById(rn).innerHTML = mc - len;
+    document.getElementById(ms).innerHTML = msg1 ;
+}
+</script>
